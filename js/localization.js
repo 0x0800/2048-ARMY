31db5b3ad343be27f0a4c0bb09b6ca7f695622aa
alessandro petrozzi <petrosh@petrosh.it>
2014-03-28 14:05:42 +0100

    first commit

---
+++ js/localization.js
@@ -0,0 +1,49 @@
+String.toLocaleString({
+    
+	"en": {
+		"%game-intro"           : "Join the numbers and get to be <strong>GENERAL!</strong>",
+		"%restart-button"       : "New Game",
+		"%keep-playing-button"  : "Keep going",
+		"%retry-button"         : "Try again",
+		"%game-won"             : "You win!",
+		"%game-over"            : "Game over!",
+		"%game-explanation"     : "<strong class=\"important\">How to play:</strong> Use your <strong>arrow keys</strong> to move the tiles. When two tiles with the same rank touch, they <strong>get promoted!</strong>",
+		"%disclaimer"           : "<strong class=\"important\">Note:</strong> This site is a spin-off of the official version of <a href=\"http://git.io/2048\">2048</a> created by <a href=\"http://gabrielecirulli.com\" target=\"_blank\">Gabriele Cirulli</a>.",
+		"%tile-legend"          : "<strong class=\"important\">Tile Legend:</strong>",
+		'%1'                    : 'private',
+    '%2'                    : 'corporal',
+    '%3'                    : 'sergeant',
+    '%4'                    : 'lieutenant',
+    '%5'                    : 'captain',
+    '%6'                    : 'major',
+    '%7'                    : 'colonel',
+    '%8'                    : 'brigadier general',
+    '%9'                    : 'major general',
+    '%10'                   : 'lieutenant general',
+    '%11'                   : 'general'
+	},
+	
+	"it": {
+		"%game-intro"           : "Unisci i numeri e diventa <strong>Generale!</strong>",
+		"%restart-button"       : "Nuova Partita",
+		"%keep-playing-button"  : "Continua",
+		"%retry-button"         : "Riprova",
+		"%game-won"             : "Hai vinto!",
+		"%game-over"            : "Hai perso!",
+		"%game-explanation"     : "<strong class=\"important\">Come giocare:</strong> Usare i <strong>tasti freccia</strong> per muovere le tessere. Quando due tessere con lo stesso grado si scontrano c&#x27;&egrave; la <strong>promozione!</strong>",
+		"%disclaimer"           : "<strong class=\"important\">Nota:</strong> Questo sito &egrave; uno spin-off della versione ufficiale di <a href=\"http://git.io/2048\">2048</a> creata da <a href=\"http://gabrielecirulli.com\" target=\"_blank\">Gabriele Cirulli</a>.",
+		"%tile-legend"          : "<strong class=\"important\">Legenda Tessere:</strong>",
+		'%1'                    : 'soldato semplice',
+    '%2'                    : 'caporale',
+    '%3'                    : 'sergente',
+    '%4'                    : 'tenente',
+    '%5'                    : 'capitano',
+    '%6'                    : 'maggiore',
+    '%7'                    : 'colonnello',
+    '%8'                    : 'brigadiere generale',
+    '%9'                    : 'maggiore generale',
+    '%10'                   : 'tenente generale',
+    '%11'                   : 'general'
+	}
+
+});
